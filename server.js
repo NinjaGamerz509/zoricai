b730042e38ab8e746fca3c7745c789aad1a12616
NinjaGamerz509 <srahemat943@gmail.com>
2026-05-15 15:06:13 +0530

    Fix CORS for Vercel

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ connectDB().then(async () => {
 });
 
 // Middleware
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', /\.vercel\.app$/, /\.netlify\.app$/], credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(limiter);
 app.set('io', io);
